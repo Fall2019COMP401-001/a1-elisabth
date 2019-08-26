8e8d49d223c48fae34e2b8467e387003bde04cc8
730083676 <730083676@730083676-pc.wireless-1x.unc.edu>
2019-08-26 14:36:32 -0400

    A1Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,34 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int numCustomers = scan.nextInt();
+		char firstName;
+		String lastName;
+		int itemsBought;
+		double numPurchased;
+		String nameOfItem;
+		double priceOfItem;
+		double itemTotalPrice;
+		double totalCost = 0;
+		String fullName;
+		
+		for(int i=0; i<numCustomers; i++) {
+			firstName = scan.next().charAt(0);
+			lastName = scan.next();
+			fullName = firstName + ". " + lastName;
+			itemsBought = scan.nextInt();
+			for(int j=0; j<itemsBought; j++) {
+				numPurchased = scan.nextDouble();
+				nameOfItem = scan.next();
+				priceOfItem = scan.nextDouble(); 
+				itemTotalPrice = numPurchased * priceOfItem;
+				totalCost = totalCost + itemTotalPrice;
+			}
+			
+			System.out.println(fullName + ": " + String.format("%.2f", totalCost));
+		}
 		
 	}
+	
 }
+
