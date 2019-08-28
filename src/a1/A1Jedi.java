0fe407272f8c91e117b2572998b2215538f3efec
730083676 <730083676@730083676-pc.wireless-1x.unc.edu>
2019-08-28 15:15:51 -0400

    A1 Jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -10,5 +10,49 @@ public class A1Jedi {
 
 		// Your code follows here.
 		
+		int numOfStoreItems = scan.nextInt();
+		String[] storeItemsArray = new String[numOfStoreItems];
+		int[] countCustomers = new int[numOfStoreItems];
+		int[] countItems = new int[numOfStoreItems];
+		
+		for(int i=0; i<numOfStoreItems; i++) {
+			String itemName = scan.next();
+			double itemPrice = scan.nextDouble();
+			storeItemsArray[i] = itemName;
+		}
+		
+		int numOfCustomers = scan.nextInt();
+		
+		for(int i=0; i<numOfCustomers; i++) {
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int cusNumOfItems = scan.nextInt();
+			for(int j=0; j<cusNumOfItems; j++) {
+				int itemQuantity = scan.nextInt();
+				String itemBoughtName = scan.next();
+				for(int k=0; k<storeItemsArray.length; k++) {
+					if(storeItemsArray[k].contentEquals(itemBoughtName)) {
+						countCustomers[k] = countCustomers[k] + 1;
+						countItems[k] = countItems[k] + itemQuantity;
+					} else {
+						countCustomers[k] = countCustomers[k] + 0;
+						countItems[k] = countItems[k] + 0;
+					}
+				}
+
+			}
+		}
+		for(int i=0; i<numOfStoreItems; i++) {
+			if(countCustomers[i] != 0 && countItems[i] != 0) {
+				System.out.println(countCustomers[i] + " customers bought " + countItems[i] + " " + storeItemsArray[i]);
+			} else {
+				System.out.println("No customers bought " + storeItemsArray[i]);
+			}
+		}
+
 	}
+	
+	//public static int countItemsBought(String name) {
+		//return int;
+	//}
 }
